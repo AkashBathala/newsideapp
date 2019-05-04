af4007eb9f7e3534167d95c76fa3c5d8584832b4
AkashBathala <akash.bathla2013@gmail.com>
2019-05-04 17:47:57 +0530

    $$

---
+++ src/app/comp/comp.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comp',
+  templateUrl: './comp.page.html',
+  styleUrls: ['./comp.page.scss'],
+})
+export class CompPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
